8754ea3babe389b156303844042c575e61f88284
roman.kovalskyi <rkovalskiy@deloitte.com>
2020-04-20 12:56:58 +0300

    the simple vue app for file uploading

---
+++ simple-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app" class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <a class="navbar-brand active" href="#">Simple app</a>
+            <!--      <a class="nav-link" href="#">Link</a>      -->
+        </nav>
+        <div class="row"></div>
+        <div class="row">
+            <div class="col-6">
+                <FileUploader></FileUploader>
+            </div>
+            <div class="col-6">Log</div>
+        </div>
+<!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    </div>
+</template>
+
+<script>
+    import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+    // import HelloWorld from './components/HelloWorld.vue'
+    import FileUploader from './components/file-uploader.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            // HelloWorld,
+            FileUploader
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ simple-app/src/components/file-uploader.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <vue-dropzone
+                    ref="myVueDropzone"
+                    id="dropzone"
+                    :options="dropzoneOptions"
+                    :useCustomSlot=true
+                    @vdropzone-file-added="fileAdded">
+                <div class="dropzone-custom-content">
+                    <h3 class="dropzone-custom-title">Drag and drop to upload content!</h3>
+                    <div class="subtitle">...or click to select a file from your computer</div>
+                </div>
+            </vue-dropzone>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import vue2Dropzone from 'vue2-dropzone';
+
+    export default {
+        name: "FileUploader",
+        components: {
+            vueDropzone: vue2Dropzone
+        },
+        data: function () {
+            return {
+                dropzoneOptions: {
+                    url: 'https://httpbin.org/post',
+                    thumbnailWidth: 150,
+                    addRemoveLinks: true
+                }
+            }
+        },
+        methods: {
+            fileAdded( file ) {
+                console.log( file );
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .dropzone-custom-title {
+        margin-top: 0;
+        color: #00b782;
+    }
+
+    .subtitle {
+        color: #314b5f;
+    }
+</style>
